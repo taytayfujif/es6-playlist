71cfb395ff0aa18aee3a46680e576a483e7869e8
taytayfujif <31020217+taytayfujif@users.noreply.github.com>
2018-09-17 14:14:52 -1000

    1st commit

---
+++ data.js
@@ -0,0 +1,34 @@
+const data = [
+    {
+        artist: 'LAUV',
+        genre: 'Pop',
+        song: 'Paris in the rain',
+        album image: 
+    }
+    {
+        artist: 'Rich Brian',
+        genre: 'hip-hop/rap',
+        song: 'cold',
+        albumImage:
+    }
+    {
+        artist: '88rising',
+        genre: 'R&B/soul',
+        song: 'Plans',
+        album image: 
+    }
+    {
+        artist: 'Shaun',
+        genre: 'electronic',
+        song: 'Way Back Home',
+        album image: 
+    }
+    {
+      artist: 'All Time Low',
+        genre: 'Alternative',
+        song: 'Great Escape',
+        album image: 
+    }
+
+
+]
